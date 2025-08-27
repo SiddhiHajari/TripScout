6aa6abed5420f0345e3b5879ce4da063c65653b2
SiddhiHajari <siddhiphajari@gmail.com>
2025-08-27 23:47:59 +0530

    Get all travel story api integration

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from "../../components/Navbar"
+import axiosInstance from "../../utils/axiosInstance"
 
 
 const Home = () => {
+  const [allStories, setAllStories] = useState([])
+  console.log(allStories)
+
+  // Get all travel stories
+  const getAllTravelStories = async () => {
+    try {
+      const response = await axiosInstance.get("/travel-story/get-all")
+
+      if (response.data && response.data.stories) {
+        setAllStories(response.data.stories)
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again.")
+    }
+  }
+
+  useEffect(() => {
+    getAllTravelStories()
+
+    return () => {}
+  }, [])
+
   return (
-    <div><Navbar/></div>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
