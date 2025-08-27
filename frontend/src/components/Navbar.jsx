726bd70db714ee35eae0da0cfb5309e9c1c18a68
SiddhiHajari <siddhiphajari@gmail.com>
2025-08-27 23:14:23 +0530

    Sign-out api integration

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import Profile from "./Profile"
+import { signOutSuccess } from "../redux/slice/userSlice"
+import axiosInstance from "../utils/axiosInstance"
+import { useDispatch } from "react-redux"
 
 const Navbar = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const onLogout = async () => {
+        try {
+        const response = await axiosInstance.post("/user/signout")
+
+        if (response.data) {
+            dispatch(signOutSuccess())
+
+            navigate("/login")
+        }
+        } catch (error) {
+        console.log(error)
+        }
+    }
   return (
     <div className="bg-white flex items-center justify-between px-10 py-2 drop-shadow sticky top-0 z-10">
         <Link to={"/"}>
@@ -10,9 +29,10 @@ const Navbar = () => {
           <span className="text-blue-400">Trip</span>
           <span className="text-blue-800">Scout</span>
         </h1>
-      </Link>
+        </Link>
 
-      <Profile  />
+        <Profile onLogout={onLogout} />
+
     </div>
   )
 }
--- frontend/src/redux/slice/userSlice.js
+++ frontend/src/redux/slice/userSlice.js
@@ -6,30 +6,35 @@ const initialState = {
   loading: null,
 }
 
-
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        signInStart: (state) => {
-        state.loading = true
-        state.error = null
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true
+      state.error = null
+    },
 
-        signInSuccess: (state, action) => {
-        state.currentUser = action.payload
-        state.loading = false
-        state.error = null
-        },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload
+      state.loading = false
+      state.error = null
+    },
 
-        signInFailure: (state, action) => {
-        state.loading = false
-        state.error = action.payload
-        },
-    }
+    signInFailure: (state, action) => {
+      state.loading = false
+      state.error = action.payload
+    },
+
+    signOutSuccess: (state) => {
+      state.currentUser = null
+      state.error = null
+      state.loading = false
+    },
+  },
 })
 
-export const { signInStart, signInSuccess, signInFailure } =
+export const { signInStart, signInSuccess, signInFailure, signOutSuccess } =
   userSlice.actions
 
 export default userSlice.reducer
